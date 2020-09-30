ab564f358e9819b62250ffd10d43b30a47ee3d52
Yoel76555 <71801083+Yoel76555@users.noreply.github.com>
2020-09-29 19:55:27 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var fixedrect, movingrect;
+
+
+
+
+function setup() {
+  createCanvas(800,400);
+  fixedrect= createSprite(200, 200, 50, 80);
+  movingrect= createSprite(400,200,80,30);
+  
+movingrect.shapeColor="green";
+fixedrect.shapeColor="green";
+
+fixedrect.debug=true;
+movingrect.debug=true;
+}
+
+function draw() {
+  background(0);  
+
+  if(movingrect.x-fixedrect.x < fixedrect.width/2+movingrect.width/2 && 
+    fixedrect.x-movingrect.x< fixedrect.width/2+movingrect.width/2 && 
+    movingrect.y-fixedrect.y< fixedrect.width/2+movingrect.width/2 && 
+    fixedrect.y-movingrect.y< fixedrect.width/2+movingrect.width/2 ){
+ movingrect.shapeColor="red";
+ fixedrect.shapeColor="red";
+
+  }
+  else {
+    movingrect.shapeColor="green";
+    fixedrect.shapeColor="green";
+
+  }
+  movingrect.x=World.mouseX;
+  movingrect.y=World.mouseY;
+
+  drawSprites();
+}
